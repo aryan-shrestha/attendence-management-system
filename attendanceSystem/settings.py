3c6720685a44a9e5f0b2c6dc3d091f3740c0ac3d
Aryan Shrestha <68954986+aryan-shrestha@users.noreply.github.com>
2022-09-04 19:35:43 +0545

    whitenoise configured

--- attendanceSystem/settings.py
+++ attendanceSystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 import django_heroku
 from pathlib import Path
 
@@ -136,6 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
@@ -148,3 +151,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
